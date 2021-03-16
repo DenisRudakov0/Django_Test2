626221024b596bfe497a11a3f3d4ac66c8f7dd1a
DenisRudakov0 <denis.rudakov.2000.14@gmail.com>
2021-03-16 16:54:44 +0300

    Bootstrap_css_v1

--- Djangotests/urls.py
+++ Djangotests/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # При переходе будет открываться панель администратора
     # Отслеживание url адресов
     path('', include('main.urls')) # указываем слудующий файл который будет отслеживать путь
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
